2c17b624a09e24003f33e5ee5dd499babd586757
Brandon Martinez <me@brandonmartinez.com>
2015-12-15 01:03:18 -0500

    Added "SqlPublishDatabaseProjectToServer" for DACPAC publishing in release tab.

---
+++ Tasks/SqlPublishDatabaseProjectToServer/Publish-SqlDatabaseProjectToServer.ps1
@@ -0,0 +1,87 @@
+[CmdletBinding(DefaultParameterSetName = 'None')]
+param
+(
+    [String] [Parameter(Mandatory = $true)]
+    $DatabasePublishProfilePath,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DatabaseDacpacPath,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DatabaseServerName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DatabaseName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DatabaseUserName,
+
+    [String] [Parameter(Mandatory = $true)]
+    $DatabasePassword,
+
+    [String]
+    $AdditionalArguments
+)
+
+Write-Host "Entering script Publish-SqlDatabaseProjectToAzure.ps1"
+
+Write-Verbose "DatabasePublishProfilePath = $DatabasePublishProfilePath"
+Write-Verbose "DatabaseDacpacPath = $DatabaseDacpacPath"
+Write-Verbose "DatabaseServerName = $DatabaseServerName"
+Write-Verbose "DatabaseName = $DatabaseName"
+Write-Verbose "DatabaseUserName = $DatabaseUserName"
+Write-Verbose "DatabasePassword = $DatabasePassword"
+Write-Verbose "AdditionalArguments = $AdditionalArguments"
+
+# Import the Task.Common and Task.Internal dll that has all the cmdlets we need for Build
+$agentWorkerModulesPath = "$($env:AGENT_HOMEDIRECTORY)\agent\worker\Modules"
+$agentDistributedTaskInternalModulePath = "$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Task.Internal\Microsoft.TeamFoundation.DistributedTask.Task.Internal.dll"
+$agentDistributedTaskCommonModulePath = "$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Task.Common\Microsoft.TeamFoundation.DistributedTask.Task.Common.dll"
+$agentDistributedTaskDevTestLabs = "$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs\Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs.dll"
+$agentDistributedTaskDeploymentInternal = "$agentWorkerModulesPath\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Internal\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Internal.psm1"
+  
+Write-Verbose "Importing VSTS Module $agentDistributedTaskInternalModulePath" 
+Import-Module $agentDistributedTaskInternalModulePath
+Write-Verbose "Importing VSTS Module $agentDistributedTaskCommonModulePath"
+Import-Module $agentDistributedTaskCommonModulePath
+Write-Verbose "Importing VSTS Module $agentDistributedTaskDevTestLabs"
+Import-Module $agentDistributedTaskDevTestLabs
+Write-Verbose "Importing VSTS Module $agentDistributedTaskDeploymentInternal"
+Import-Module $agentDistributedTaskDeploymentInternal
+
+$scriptArgument = Get-SqlPackageCommandArguments -dacpacFile $DatabaseDacpacPath -targetMethod "server" -serverName $DatabaseServerName -databaseName $DatabaseName -sqlUsername $DatabaseUserName -sqlPassword $DatabasePassword -publishProfile $DatabasePublishProfilePath -additionalArguments $AdditionalArguments
+
+Write-Verbose "Created SQLPackage.exe agruments"  -Verbose
+
+<#
+// Returns an older version of sqlpackage; working around this for now
+$sqlDeploymentScriptPath = Join-Path "$env:AGENT_HOMEDIRECTORY" "Agent\Worker\Modules\Microsoft.TeamFoundation.DistributedTask.Task.DevTestLabs\Scripts\Microsoft.TeamFoundation.DistributedTask.Task.Deployment.Sql.ps1"
+$SqlPackageCommand = "& `"$sqlDeploymentScriptPath`" $scriptArgument"
+
+Write-Verbose "Executing SQLPackage.exe"  -Verbose
+
+$ErrorActionPreference = 'Continue'
+
+Invoke-Expression -Command $SqlPackageCommand
+
+$ErrorActionPreference = 'Stop'
+#>
+
+Write-Verbose "Executing SQLPackage.exe"  -Verbose
+$sqlPackage = "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\Extensions\Microsoft\SQLDB\DAC\120\SqlPackage.exe"
+$sqlPackageArguments = $scriptArgument.Trim('"', ' ')
+$command = "`"$sqlPackage`" $sqlPackageArguments"
+$command = $command.Replace("'", "`"")
+$PowershellVersion5 = "5"
+$ErrorActionPreference = 'Continue'
+if ($PSVersionTable.PSVersion.Major -ge $PowershellVersion5) {
+    cmd.exe /c "$command"
+} else {
+    cmd.exe /c "`"$command`""
+}
+$ErrorActionPreference = 'Stop'
+
+
+
+
+Write-Host "Leaving script Publish-SqlDatabaseProjectToAzure.ps1"
