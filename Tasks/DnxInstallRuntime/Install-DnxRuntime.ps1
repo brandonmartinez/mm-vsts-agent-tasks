62dc3504c193930baa44bc026c9395e0720c32cf
Brandon Martinez <me@brandonmartinez.com>
2016-09-16 11:39:15 -0400

    Pointed dnvm install to personal fork for legacy filler

--- Tasks/DnxInstallRuntime/Install-DnxRuntime.ps1
+++ Tasks/DnxInstallRuntime/Install-DnxRuntime.ps1
@@ -12,7 +12,7 @@ $GlobalJsonPath = Join-Path $SolutionRoot "global.json"
 Write-Verbose "SolutionRoot = $SolutionRoot"
 Write-Verbose "GlobalJsonPath = $GlobalJsonPath"
 
-&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
+&{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/brandonmartinez/Aspnet-Home/dev/dnvminstall.ps1'))}
 $globalJson = Get-Content -Path $GlobalJsonPath -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
 $dnxVersion = if($globalJson -and $globalJson.sdk -and $globalJson.sdk.version) { $globalJson.sdk.version } else { throw("Global.json doesn't specify DNX runtime version.") }
 $dnxArchitecture = if($globalJson -and $globalJson.sdk -and $globalJson.sdk.architecture) { $globalJson.sdk.architecture } else { "x86" }
